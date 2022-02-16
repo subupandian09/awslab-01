a307df736f672d65543d61c74c7455402fe187f3
Pandian <247265@USREMWFH1004655.lan>
2022-02-15 22:51:53 -0600

    secound update

---
+++ output.tf
@@ -0,0 +1,18 @@
+output "aws_route53_zone.public_zone.zone_id" {
+value=aws_route53_zone.public_zone.zone_id
+description="aws_route53_zone.public_zone.zone_id"
+}
+
+output "ws_acm_certificate.default.domain_validation_options.0.resource_record_name"
+value=ws_acm_certificate.default.domain_validation_options.0.resource_record_name
+description="ws_acm_certificate.default.domain_validation_options.0.resource_record_name"
+}
+
+
+output "aws_acm_certificate.default.domain_validation_options.0.resource_record_value" {
+value=aws_acm_certificate.default.domain_validation_options.0.resource_record_value
+decription="aws_acm_certificate.default.domain_validation_options.0.resource_record_value"
+}
+
+
+
